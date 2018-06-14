9835c5cedd44785faf31dc730bf6bc8acb6decfc
Michael Menzies <michael@dwarfstar.co.nz>
2018-06-14 12:07:21 +1200

    Add proxy and unregister ServiceWorker for sane caching

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import './index.css';
 
+unregister() ;
+
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
